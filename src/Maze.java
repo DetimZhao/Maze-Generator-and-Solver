5afc14a3057faa085679508edcbeb2eab2f8c042
DetimZhao <detimzhao8668@gmail.com>
2023-03-24 14:29:58 -0700

    Basic start to maze, updated gitignore

--- src/Maze.java
+++ src/Maze.java
@@ -1,6 +1,21 @@
 
 public class Maze {
 
+    private int width;
+    private int height;
+    private int[][] cells;
 
+    // First constructor has the same size (w and l) for the maze
+    public Maze(int size) {
+        // this keyword to call the other constructor
+        this(size, size);
+    }
+
+    // Second constructor if user wants to define w and l for the maze
+    private Maze(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.cells = new int[width][height];
+    }
 
 }
